ff4b35bafa6fc560c8c832f3019a4630ae07bc8e
QinYYY <qinyang_c@outlook.com>
2024-09-09 17:40:43 +0930

    use spm use local notification use share link use swiftdata learn how to get QR Code from string

---
+++ HotProspects/Prospect.swift
@@ -0,0 +1,8 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by QinY on 9/9/2024.
+//
+
+import Foundation
---
+++ HotProspects/ProspectsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by QinY on 9/9/2024.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProspectsView()
+}
